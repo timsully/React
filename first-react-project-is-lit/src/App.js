e5d365f80cbec6bb847876267e86c60f32c14479
Tim Sullivan <timsullivan97@gmail.com>
2018-08-18 20:50:54 -0700

    adding updateEntry to save item to certain day

--- first-react-project-is-lit/src/App.js
+++ first-react-project-is-lit/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Importing the React class and ReactDOM
+import React from 'React'
+import ReactDOM from 'react-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+import { Header } from './Components/Header';
+import { Textbox } from './Components/Textbox';
+import { DisplayToday, DisplayTomorrow, DisplayDayAfterTomorrow } from './Components/Display';
+
+// App component which is our parent component
+class App extends React.component{
+  constructor(props){
+    super(props);
+    this.state = {
+      'Today' : [],
+      'Tomorrow' : [],
+      'Day_After_Tomorrow' : [],
+      'username' : 'yo buddy!'
+    }
+    this.updateEntry = this.updateEntry.bind(this);
+    this.deleteEntry = this.deleteEntry.bind(this);
   }
 }
-
-export default App;
